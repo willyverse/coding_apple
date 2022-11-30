40f9f7b62fb2bda08c9f43c909ac9d4acc271546
Chanyoung Kim <whqnd1106@gmail.com>
2022-11-30 23:41:36 +0900

    nodejs post ~ mongodb insert

--- node_mongo/server.js
+++ node_mongo/server.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoConfig = require('./mongoConfig.json');
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(8080, function () {
-    console.log('welcome to 8080');
-});
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(`mongodb+srv://${mongoConfig.id}:${mongoConfig.password}@codingapple.ofswmdl.mongodb.net/?retryWrites=true&w=majority`,
+    (error, client) => {
+        if (error) { return console.log(error); }
+
+        app.listen(8080, function () {
+            console.log('welcome to 8080');
+            console.log(mongoConfig.id, mongoConfig.password);
+        });
+    });
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/hello', (req, res) => {
-    res.send('welcome');
+app.get('/write', (req, res) => {
+    res.sendFile(__dirname + "/write.html");
+});
+
+app.post('/add', (req, res) => {
+    res.send(`title: ${req.body.title} date: ${req.body.date}`);
+    MongoClient.connect(`mongodb+srv://${mongoConfig.id}:${mongoConfig.password}@codingapple.ofswmdl.mongodb.net/?retryWrites=true&w=majority`,
+        (error, client) => {
+            if (error) { return console.log(error); }
+
+            db = client.db('todoapp');
+
+            db.collection('post').insertOne({ title: req.body.title, date: req.body.date }, (error, result) => {
+                console.log('저장완료');
+            });
+        })
 });
