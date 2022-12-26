195c1d63e245c29e511c0ccaf26337eebe7834a8
Chanyoung Kim <whqnd1106@gmail.com>
2022-12-26 23:15:12 +0900

    mongodb find, insert, update

--- node_mongo/server.js
+++ node_mongo/server.js
@@ -3,8 +3,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongoConfig = require('./mongoConfig.json');
 app.use(bodyParser.urlencoded({ extended: true }));
-
 const MongoClient = require('mongodb').MongoClient;
+app.set('view engine', 'ejs');
 
 MongoClient.connect(`mongodb+srv://${mongoConfig.id}:${mongoConfig.password}@codingapple.ofswmdl.mongodb.net/?retryWrites=true&w=majority`,
     (error, client) => {
@@ -14,6 +14,8 @@ MongoClient.connect(`mongodb+srv://${mongoConfig.id}:${mongoConfig.password}@cod
             console.log('welcome to 8080');
             console.log(mongoConfig.id, mongoConfig.password);
         });
+
+        db = client.db('todoapp');
     });
 
 app.get('/', (req, res) => {
@@ -26,14 +28,28 @@ app.get('/write', (req, res) => {
 
 app.post('/add', (req, res) => {
     res.send(`title: ${req.body.title} date: ${req.body.date}`);
-    MongoClient.connect(`mongodb+srv://${mongoConfig.id}:${mongoConfig.password}@codingapple.ofswmdl.mongodb.net/?retryWrites=true&w=majority`,
-        (error, client) => {
-            if (error) { return console.log(error); }
+    db.collection('counter').findOne({ name: 'totalPost' }, (error, result) => {
+        console.log(result.totalPost);
+        var currentIndex = result.totalPost;
 
-            db = client.db('todoapp');
+        db.collection('post').insertOne({ _id: currentIndex + 1, title: req.body.title, date: req.body.date }, (error, result) => {
+            console.log('저장완료');
 
-            db.collection('post').insertOne({ title: req.body.title, date: req.body.date }, (error, result) => {
-                console.log('저장완료');
-            });
-        })
+            db.collection('counter').updateOne({ name: 'totalPost' }, { $set: { totalPost: currentIndex + 1 } }, (error, result) => {
+                // {$inc: {totalPost: 1}} 해도 동일한 표현임, $inc는 increase.
+                if (error) { return console.log(error) };
+            })
+        });
+    })
+
+
 });
+
+// /list로 GET 요청하면 실제 DB에 저장된 데이터들로 예쁘게 꾸며진 HTML을 보여줌
+app.get('/list', (req, res) => {
+
+    db.collection('post').find().toArray((err, data) => {
+        console.log(data);
+        res.render('list.ejs', { data: data });
+    });
+})
