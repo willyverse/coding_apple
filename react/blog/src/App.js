ee48189934d7e59c975737c275fab4da82299d89
Chanyoung Kim <whqnd1106@gmail.com>
2023-02-10 01:12:39 +0900

    state onclick component props Array

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -3,10 +3,13 @@
 import './App.css';
 import { useState } from 'react';
 
-function App() {
+let App = () => {
 
-  let [title, setTitle] = useState(['남자 코트 추천', '강남 맛집 추천', '여자 머리 추천']);
-  let [count, setCount] = useState(0);
+  let [title, setTitle] = useState(['남자 코트 추천', '강남 맛집 추천', '여자 머리 추천', '캠핑 장소 추천']);
+  const countArray = new Array(title.length).fill(0);
+  let [count, setCount] = useState(countArray);
+  const modalArray = new Array(title.length).fill(false);
+  let [modal, setModal] = useState(modalArray);
 
   return (
     <div className="App">
@@ -18,22 +21,65 @@ function App() {
           copy.sort()
           setTitle(copy);
         }}>가나다순 정렬</button>
+      </div>
 
-      </div>
-      <div className="list">
-        <h4>{title[0]} <span onClick={() => { setCount(count + 1) }}>😍</span> {count} </h4>
-        <p>2월 8일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[1]}</h4>
-        <p>2월 8일 발행</p>
-      </div>
-      <div className="list">
-        <h4>{title[2]}</h4>
-        <p>2월 8일 발행</p>
-      </div>
+      {
+        title.map(function (a, i) {
+          return (
+            <div className="list">
+              <h4 style={{ display: 'flex' }}>
+                <span onClick={() => {
+                  let copyModal = [...modal];
+                  if (modal[i]) {
+                    copyModal[i] = false;
+                    setModal(copyModal);
+                  } else {
+                    copyModal[i] = true;
+                    setModal(copyModal);
+                  }
+                }}>{title[i]}
+                </span>
+                <span onClick={() => {
+                  let countCopy = [...count];
+                  countCopy[i] += 1;
+                  setCount(countCopy);
+                }}>
+                  &nbsp;😍
+                </span>
+                {count[i]}
+              </h4>
+              <p>2월 8일 발행</p>
+              {
+                modal[i] ? <Modal title={title} num={i} setTitle={setTitle} /> : null
+              }
+            </div>
+          )
+        })
+      }
     </div>
   );
+}
+
+let Modal = (props) => {
+  let title = props.title;
+  let num = props.num;
+  return (
+    <div className='modal'>
+      <h4>{title[num]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button onClick={() => {
+        let copyTitle = [...title];
+        if (num === title.length - 1) {
+          copyTitle[num] = title[0];
+          props.setTitle(copyTitle);
+        } else {
+          copyTitle[num] = title[num + 1];
+          props.setTitle(copyTitle);
+        }
+      }}>글수정</button>
+    </div>
+  )
 }
 
 export default App;
